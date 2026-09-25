fff349d765967d36740b384c57dde3b676892e0e
Arman Emp <74330704+armanemp@users.noreply.github.com>
2026-09-26 01:39:27 +0330

    fix: restore single panel alignment implementation

--- integrations/ctrader/indicators/CFIP_MTF_LiveEntryEngine_Clean_v30.cs
+++ integrations/ctrader/indicators/CFIP_MTF_LiveEntryEngine_Clean_v30.cs
@@ -10456,6 +10456,44 @@ namespace cAlgo
                 : frame.Direction;
         }
 
+        private void SetPanelAlignment()
+        {
+            VerticalAlignment vertical;
+            HorizontalAlignment horizontal;
+
+            switch (PanelPosition)
+            {
+                case CFIPClean30PanelCorner.TopLeft:
+                    vertical = VerticalAlignment.Top;
+                    horizontal = HorizontalAlignment.Left;
+                    break;
+
+                case CFIPClean30PanelCorner.TopRight:
+                    vertical = VerticalAlignment.Top;
+                    horizontal = HorizontalAlignment.Right;
+                    break;
+
+                case CFIPClean30PanelCorner.BottomRight:
+                    vertical = VerticalAlignment.Bottom;
+                    horizontal = HorizontalAlignment.Right;
+                    break;
+
+                default:
+                    vertical = VerticalAlignment.Bottom;
+                    horizontal = HorizontalAlignment.Left;
+                    break;
+            }
+
+            if (_panel != null)
+            {
+                _panel.VerticalAlignment =
+                    vertical;
+
+                _panel.HorizontalAlignment =
+                    horizontal;
+            }
+        }
+
         private string GetStablePanelState(
             string candidate)
         {
